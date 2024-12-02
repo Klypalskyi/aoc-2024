dd01b85f108e54a7ee65d55db1667933806c0208
Mykhailo Klypalskyi <klypalskyi@gmail.com>
2024-12-02 11:26:48 +0100

    2.2

--- 2/index.js
+++ 2/index.js
@@ -1,19 +1,11 @@
-const { fetchInputByDay } = require('../utils');
+const { fetchInputByDay } = require("../utils");
 
 const parseInputFromText = (text) => {
-  const arr = text.split('\n').filter((line) => line.trim() !== '').map(line => line.split(' ').map(Number));
+  const arr = text
+    .split("\n")
+    .filter((line) => line.trim() !== "")
+    .map((line) => line.split(" ").map(Number));
   return arr;
-};
-
-const isArrIncreasing = (arr) => {
-  let increasing = true;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > arr[i + 1]) {
-      increasing = false;
-      break;
-    }
-  }
-  return increasing;
 };
 
 const isReportSafe = (arr) => {
@@ -22,20 +14,42 @@ const isReportSafe = (arr) => {
     diffs.push(arr[i + 1] - arr[i]);
   }
 
-  const areDiffsValid = diffs.every(diff => Math.abs(diff) >= 1 && Math.abs(diff) <= 3 && diff !== 0)
+  const areDiffsValid = diffs.every(
+    (diff) => Math.abs(diff) >= 1 && Math.abs(diff) <= 3 && diff !== 0
+  );
   const areDiffsIncreasing = diffs.every((diff) => diff > 0);
   const areDiffsDecreasing = diffs.every((diff) => diff < 0);
 
   return areDiffsValid && (areDiffsIncreasing || areDiffsDecreasing);
-}
+};
 
+const tryToMakeReportSafe = (arr) => {
+  let foundSafe = false;
+  for (let i = 0; i < arr.length; i++) {
+    const updatedArr = arr.slice(0, i).concat(arr.slice(i + 1));
 
-const task1 = async() => {
+    if (isReportSafe(updatedArr)) {
+      foundSafe = true;
+      break;
+    }
+  }
+  return foundSafe;
+};
+
+const task1 = async () => {
   const input = await fetchInputByDay(2);
   const parsedInput = parseInputFromText(input);
   const safeReports = parsedInput.filter(isReportSafe);
+  const unSafeReports = parsedInput.filter((report) => !isReportSafe(report));
 
-  console.log('2.1', safeReports.length);
+  console.log("2.1", safeReports.length);
+  return { safeReports, unSafeReports };
 };
 
-task1();
+const task2 = async () => {
+  const { safeReports, unSafeReports } = await task1();
+  const newSafeReports = unSafeReports.filter(tryToMakeReportSafe);
+  console.log("2.2", safeReports.length + newSafeReports.length);
+};
+
+task2();

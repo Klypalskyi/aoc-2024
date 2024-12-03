430ec1112e49c6199b47dcf8a6d79bef12e2babc
Mykhailo Klypalskyi <klypalskyi@gmail.com>
2024-12-03 09:35:39 +0100

    3.2

--- 3/index.js
+++ 3/index.js
@@ -1,17 +1,29 @@
 const { fetchInputByDay } = require("../utils");
-
 const regexRule = /mul\(\d{1,3},\d{1,3}\)/g;
 
+const accumulate = (arr) => arr.reduce((acc, item) => {
+  const [a, b] = item.match(/\d{1,3}/g);
+  return acc + a * b;
+}, 0);
 
 const task1 = async () => {
   const input = await fetchInputByDay(3);
   const filtered = input.match(regexRule);
-  const result = filtered.reduce((acc, item) => {
-    const [a, b] = item.match(/\d{1,3}/g);
-    return acc + a * b;
-  }, 0);
+  const result = accumulate(filtered)
 
-  console.log(result)
+  console.log('3.1', result);
+};
+
+const task2 = async () => {
+  const input = await fetchInputByDay(3);
+  const ignoreRegex = /don'?t\(\).*?do\(\)/gs;
+  const relevantText = input.split(ignoreRegex).join("");
+
+  const filtered = relevantText.match(regexRule);
+  const result = accumulate(filtered)
+
+  console.log('3.2', result);
 };
 
 task1();
+task2();
